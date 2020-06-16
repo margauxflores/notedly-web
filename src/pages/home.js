dff8e22604125aad001861e67b3ec38df5a1f4ef
Margaux Flores <margaux.flores@bitflyer.com>
2020-06-16 23:04:01 +0900

    Add more components

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Button from '../components/Button';
 
 const Home = () => {
   return (
     <div>
-      <h1>Notedly</h1>
       <p>This is the home page</p>
-      { /* Add a list of links*/ }
-      <ul>
-        <li>
-          <Link to="/mynotes">My Notes</Link>
-        </li>
-        <li>
-          <Link to="/favorites">Favorites</Link>
-        </li>
-      </ul>
+      <Button>Click Me!</Button>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//Import Shared Layout Component
+import Layout from '../components/Layout';
+
+//Import routes
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
@@ -7,9 +12,11 @@ import Favorites from './favorites';
 const Pages = () => {
   return (
     <Router>
-      <Route exact path="/" component={Home} />
-      <Route path="/mynotes" component={MyNotes} />
-      <Route path="/favorites" component={Favorites} />
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <Route path="/mynotes" component={MyNotes} />
+        <Route path="/favorites" component={Favorites} />
+      </Layout>
     </Router>
   );
 };
